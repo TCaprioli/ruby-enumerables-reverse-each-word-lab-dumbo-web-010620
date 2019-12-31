d9b561b9d7642c9026b5d271e10264823759c012
Tyler Caprioli <capriolityler@gmail.com>
2019-12-31 11:49:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
   string_array << string.split
   
    string_array.each do |string_array| 
-     new_string = string_array.reverse
-   
+      string_array.reverse
+      
   end
-  new_string
+  
 end  
